98c940a9456576d6708f14fcdc3f78ae56d42f9f
unknown <ezzheva.ira@yandex.ru>
2023-07-24 17:25:57 +0300

    куки

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { celebrate, Joi } = require('celebrate');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { errors } = require('celebrate');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cookieParser = require('cookie-parser');
 
 const { PORT = 3000 } = process.env;
 
@@ -10,15 +16,33 @@ const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use(auth);
+app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), createUser);
+
+app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use('*', (_req, res) => res.status(404).json({ message: 'Страница не найдена' }));
 
+/** централизованный обработчик ошибок */
+app.use(errors()); // обработчик ошибок celebrate
 app.use((err, _req, res, next) => {
   /** если у ошибки нет статуса выставляем 500 */
   const { statusCode = 500, massage } = err;
--- controllers/users.js
+++ controllers/users.js
@@ -87,10 +87,8 @@ module.exports.updateAvatar = (req, res, next) => {
     .then((user) => {
       res.send(user);
     })
-    // eslint-disable-next-line consistent-return
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        // eslint-disable-next-line no-undef
         next(new BadRequest('Некорректные данные пользователя'));
       }
     });
@@ -100,8 +98,12 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.send({ token });
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key');
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true,
+      });
     })
     .catch(next);
 };
